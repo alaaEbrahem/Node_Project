6535b0af87be5d8e66674ebf7191bf2754478af2
Alaa_Ebrahim247 <alaaebrahemelsayed247@gmail.com>
2019-04-27 15:57:16 +0200

    ctegory added

--- routes/categories.js
+++ routes/categories.js
@@ -1,51 +1,35 @@
 var express = require('express');
 var router = express.Router();
-const createError = require('http-errors');
-const categoryModel = require('./../Model/category');
+const createError=require('http-errors');
 
 
+const categoryModel=require('../Model/category');
 
-router.get('/', function (req, res, next) {
-  categoryModel.find((err, data) => {
-    res.send(data)
-  })
+router.get('/',(req,res,err)=>{
+  categoryModel.find({})
+    .then(categories=>res.send(categories))
+    .catch(err=>next(createError(500,err.message)))
+    
 });
 
-
-router.get('/create', (req, res) => {
-  res.send('createCategory')
-})
-
-
-
-router.post('/create', (req, res, next) => {
-  const category = new categoryModel(req.body)
-  category
-    .save()
-    .then(category => res.send(category))
-    .catch(err => next(createError(400, err.message)));
+router.post('/', /*async*/(req, res, next)=> {
+    
+    /*await*/ categoryModel.create(req.body)
+    .then(msgs=>res.send("done"))
+    .catch(err=>next(createError(400,err.message)))
 });
 
-router.patch('/:Id', (req, res, next) => {
-  categoryModel
-    .findByIdAndUpdate(req.params.Id, req.body, { new: true })
-    .then(user => res.send(user))
-    .catch(err => next(createError(400, err.message)))
-});
+router.patch('/:id', function(req, res, next){
+    debugger
+    categoryModel.findByIdAndUpdate(req.params.id,req.body)
+    .then(msgs=>res.send("Updated"))
+    .catch(err=>next(createError(500,err.message)));
+  });
 
-router.delete('/:Id', (req, res, next) => {
-  categoryModel
-    .findByIdAndDelete(req.params.Id)
-    .then(c => res.send(c))
-    .catch(err => next(createError(400, err.message)))
-});
-
-router.get('/:Id', (req, res, next) => {
-  categoryModel
-    .findById(req.params.Id)
-    .then(c => res.send(c))
-    .catch(err => next(createError(404, err.message)));
-})
-
-
+  router.delete('/:id', function(req, res, next){
+    debugger
+    categoryModel.findByIdAndDelete(req.params.id)
+    .then(msgs=>res.send('deleted'))
+    .catch(err=>next(createError(500,err.message)));
+ });
 module.exports = router;
